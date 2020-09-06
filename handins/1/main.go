dc0ddfa76aeb1325e9ea5b44e2749b41d81001ea
Lukas Forst <lukas@forst.pw>
2020-09-06 15:20:09 +0200

    finalize HW1

--- handins/1/main.go
+++ handins/1/main.go
@@ -9,46 +9,37 @@ import (
 	"strings"
 )
 
-func printIncoming(str string) {
-	fmt.Printf("< %s\n> ", str)
-}
-
-func printStatus(str string) {
-	fmt.Printf("- %s\n> ", str)
-}
-
-func newConnection(conn net.Conn, cons *Model) {
+func OnNewConnection(conn net.Conn, cons *Model) {
 	cons.AddConn(conn)
 	defer cons.RemoveAndCloseConn(conn)
 
 	for {
 		msg, err := bufio.NewReader(conn).ReadString('\n')
 		if err != nil {
-			log.Println("Client disconnected.")
 			break
 		}
 
 		if !cons.WasProcessed(msg) {
 			cons.MessageProcessed(msg)
-			printIncoming(strings.TrimSpace(msg))
+			PrintIncoming(strings.TrimSpace(msg))
 			go cons.BroadCastString(msg)
 		}
-		fmt.Print("> ")
 	}
-	log.Printf("Exiting newConnection")
 }
 
-func runReader(cons *Model) {
+func RunReader(cons *Model) {
 	for {
 		msg, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		text := msg
-		// TODO maybe check whether was processed?
-		cons.MessageProcessed(text)
-		go cons.BroadCastString(text)
+		if strings.TrimSpace(msg) == "quit" {
+			break
+		}
+		cons.MessageProcessed(msg)
+		go cons.BroadCastString(msg)
+		fmt.Print("> ")
 	}
 }
 
-func runServer(cons *Model) {
+func RunServer(cons *Model) {
 	ln, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal(err)
@@ -58,39 +49,37 @@ func runServer(cons *Model) {
 	ipAddress := GetOutboundIP()
 	_, port, _ := net.SplitHostPort(ln.Addr().String())
 	ipAndPort := ipAddress + ":" + port
-	printStatus("Listening for connections on IP:port " + ipAndPort)
+	PrintStatus("Listening for connections on IP:port " + ipAndPort)
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Println("Connection failed - " + err.Error())
+			PrintStatus("Connection failed - " + err.Error())
 			continue
 		}
-		go newConnection(conn, cons)
+		go OnNewConnection(conn, cons)
 	}
 }
 
-func startup() {
+func Startup() {
 	cons := MakeModel()
 
-	printStatus("Enter IP address and the port of the peer.")
+	PrintStatus("Enter IP address and the port of the peer.")
 	stdinReader := bufio.NewReader(os.Stdin)
 	ipPort, _ := stdinReader.ReadString('\n')
 
 	conn, err := net.Dial("tcp", strings.TrimSpace(ipPort))
 
 	if err != nil {
-		printStatus("It was not possible to connect - creating own network.")
+		PrintStatus("It was not possible to connect - creating own network.")
 	} else {
-		go newConnection(conn, &cons)
+		go OnNewConnection(conn, &cons)
 	}
 
-	go runServer(&cons)
-	go runReader(&cons)
-	for {
-	}
+	go RunServer(&cons)
+	RunReader(&cons)
 }
 
 func main() {
-	startup()
+	Startup()
 }
--- handins/1/utils.go
+++ handins/1/utils.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 )
+
+func PrintIncoming(str string) {
+	fmt.Printf("< %s\n> ", str)
+}
+
+func PrintStatus(str string) {
+	fmt.Printf("- %s\n> ", str)
+}
 
 // GetOutboundIP preferred outbound ip of this machine
 // based on code taken from https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go/37382208#37382208
