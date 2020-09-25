dadc6f7d886381a97311c54cd903749c2f76d72c
Hannah Schaible <h.schaible@campus.lmu.de>
2020-09-25 15:50:02 +0200

    adjusted funcrions and added iv generation and main method

--- handins/3/aes.go
+++ handins/3/aes.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	// 	"crypto/aes"
+	"crypto/aes"
 	"crypto/rand"
+	"fmt"
+	"io"
 )
 
 type File struct {
@@ -11,20 +13,60 @@ type File struct {
 }
 
 //Write ciphertext to given file
-func EncryptToFile(key []byte, filename File) File {
+func EncryptToFile(key []byte, filename string, iv []byte) string {
 
-	return filename
+	var fileContent string
+
+	return fileContent
 }
 
 //Decrypt ciphertext from file and output plaintext
-func DecryptFromFile() {
+func DecryptFromFile(key []byte, filename string, iv []byte) string {
 
+	var fileContent string
+
+	return fileContent
 }
 
-func AesKeyGen() {
+//generate 32 byte random key
+func AesKeyGen() []byte {
 	key := make([]byte, 32)
 	_, err := rand.Read(key)
 	if err != nil {
 		//panic(fmt.Sprintf())
 	}
+	return key
+}
+
+//geerate the IV nonce
+func IVGen() []byte {
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		//panic(fmt.Sprintf)
+	}
+	return iv
+
+}
+
+func main() {
+
+	key := AesKeyGen()
+
+	filename := ""  //determine filename from input?
+	plaintext := "" //read plaintext from file
+
+	iv := IVGen()
+
+	if len(plaintext)%aes.BlockSize != 0 {
+		//panic or do padding?
+	}
+
+	fmt.Println("Original Plaintext:", plaintext)
+
+	ciphertext := EncryptToFile(key, filename, iv)
+	fmt.Println("Encrypted ciphertext: ", ciphertext)
+
+	decryptedText := DecryptFromFile(key, ciphertext, iv)
+	fmt.Println("Decrypted Plaintext: ", decryptedText)
+
 }
