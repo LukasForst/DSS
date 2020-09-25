6fbd07fb8970435369172f2b04fd7ec470c420de
Hannah Schaible <h.schaible@campus.lmu.de>
2020-09-25 15:20:28 +0200

    added Key generation function

--- handins/3/aes.go
+++ handins/3/aes.go
@@ -1,19 +1,30 @@
 package main
 
-// import (
-// 	"crypto/aes"
-// )
+import (
+	// 	"crypto/aes"
+	"crypto/rand"
+)
 
 type File struct {
 	filename string
+	content  string
 }
 
 //Write ciphertext to given file
-func EncryptToFile() {
+func EncryptToFile(key []byte, filename File) File {
 
+	return filename
 }
 
 //Decrypt ciphertext from file and output plaintext
 func DecryptFromFile() {
 
+}
+
+func AesKeyGen() {
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		//panic(fmt.Sprintf())
+	}
 }
