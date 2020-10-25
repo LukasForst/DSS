0693e42c33f4b62be1b600e31d6d7713fb5a0e3e
Hannah Schaible <h.schaible@campus.lmu.de>
2020-10-25 13:18:38 +0100

    added new function SignedTransaction

--- handins/6/requests.go
+++ handins/6/requests.go
@@ -61,3 +61,15 @@ type SignedTransaction struct {
 	Amount    int    // Amount to transfer
 	Signature string // Potential signature coded as string
 }
+
+func (l *Ledger) SignedTransaction(t *SignedTransaction) {
+	l.lock.Lock() ; defer l.lock.Unlock()
+	/* We verify that the t.Signature is a valid RSA
+	* signature on the rest of the fields in t under
+	* the public key t.From.
+	*/
+	validSignature := true
+	if validSignature {
+	l.Accounts[t.From] -= t.Amount
+	l.Accounts[t.To] += t.Amount
+	}
