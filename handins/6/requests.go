c5a9739b8a3c7d0ecf919a1deea67db98e1d6434
Hannah Schaible <h.schaible@campus.lmu.de>
2020-10-25 13:19:51 +0100

    adjusted SignedTransaction

--- handins/6/requests.go
+++ handins/6/requests.go
@@ -63,13 +63,15 @@ type SignedTransaction struct {
 }
 
 func (l *Ledger) SignedTransaction(t *SignedTransaction) {
-	l.lock.Lock() ; defer l.lock.Unlock()
+	l.lock.Lock()
+	defer l.lock.Unlock()
 	/* We verify that the t.Signature is a valid RSA
 	* signature on the rest of the fields in t under
 	* the public key t.From.
-	*/
+	 */
 	validSignature := true
 	if validSignature {
-	l.Accounts[t.From] -= t.Amount
-	l.Accounts[t.To] += t.Amount
+		l.Accounts[t.From] -= t.Amount
+		l.Accounts[t.To] += t.Amount
 	}
+}
