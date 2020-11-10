8042b2e39df3923f3831705546d78a10f81274bf
Lukas Forst <lukas@forst.pw>
2020-11-10 22:53:36 +0100

    add documnetation

--- handins/8/test.go
+++ handins/8/test.go
@@ -5,11 +5,13 @@ import (
 	cryptoRand "crypto/rand"
 	"crypto/rsa"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Account struct {
@@ -25,7 +27,7 @@ func GenerateAccount(amount int) Account {
 	return Account{privateKey, amount}
 }
 
-func GetEncoder(id int, stdinReader *bufio.Reader) *json.Encoder {
+func GetEncoder(id int, stdinReader *bufio.Reader) *net.Conn {
 	PrintStatus("Enter IP address and the port of the peer # " + strconv.Itoa(id))
 	ipPort, _ := stdinReader.ReadString('\n')
 	// connect to the peer
@@ -33,21 +35,33 @@ func GetEncoder(id int, stdinReader *bufio.Reader) *json.Encoder {
 	if err != nil {
 		log.Fatal("It was not possible to connect.")
 	}
-	return json.NewEncoder(conn)
+	return &conn
 }
 
-func SetupAccounts(enc *json.Encoder, accounts []Account) {
+func SetupAccounts(c *net.Conn, accounts []Account) {
 	for _, account := range accounts {
 		accountId, _ := json.Marshal(account.PK.PublicKey)
 		dto := MakeAccountSetupDto(AccountSetup{AccountId: string(accountId), Amount: account.amount})
-
-		if err := enc.Encode(dto); err != nil {
-			log.Fatal("Error while sending data -> " + err.Error())
-		}
+		SendJson(c, dto)
+		// sleep otherwise the json encoder on the other side will break
+		time.Sleep(50 * time.Millisecond)
 	}
 }
 
-func RunTransaction(transactionId string, from *Account, to *Account, amount int, enc *json.Encoder) {
+func SendJson(c *net.Conn, d interface{}) {
+	bytes, err := json.Marshal(d)
+	if err != nil {
+		log.Fatal("Error while sending data -> " + err.Error())
+	}
+
+	_, err = (*c).Write(bytes)
+	if err != nil {
+		log.Fatal("Error while sending data -> " + err.Error())
+	}
+
+}
+
+func RunTransaction(transactionId string, from *Account, to *Account, amount int, c *net.Conn) {
 	fromPk, _ := json.Marshal(from.PK.PublicKey)
 	toPk, _ := json.Marshal(to.PK.PublicKey)
 
@@ -60,8 +74,15 @@ func RunTransaction(transactionId string, from *Account, to *Account, amount int
 	transaction.ComputeAndSetSignature(from.PK)
 
 	dto := MakeTransactionDto(&transaction)
-	if err := enc.Encode(dto); err != nil {
-		log.Fatal("Error while sending data -> " + err.Error())
+	SendJson(c, dto)
+}
+
+func StartExecution(even bool, testId string, from *Account, to *Account, peer *net.Conn) {
+	for i := 0; i < from.amount*2; i++ {
+		if (i%2 == 0 && even) || (i%2 == 1 && !even) {
+			transactionId := fmt.Sprintf("%s-%04d", testId, i)
+			RunTransaction(transactionId, from, to, 1, peer)
+		}
 	}
 }
 
@@ -74,7 +95,7 @@ func StartTest() {
 	peer1 := GetEncoder(1, stdinReader)
 	peer2 := GetEncoder(2, stdinReader)
 
-	A := GenerateAccount(1000)
+	A := GenerateAccount(20)
 	B := GenerateAccount(0)
 	C := GenerateAccount(0)
 	accounts := []Account{A, B, C}
@@ -85,16 +106,15 @@ func StartTest() {
 	PrintStatus("Pres enter to start the execution.")
 	_, _ = stdinReader.ReadString('\n')
 
-	for i := 0; i < A.amount; i++ {
-		go RunTransaction(testId+strconv.Itoa(i), &A, &B, 1, peer1)
-		go RunTransaction(testId+strconv.Itoa(i), &A, &C, 1, peer2)
-	}
+	go StartExecution(true, testId, &A, &B, peer1)
+	go StartExecution(false, testId, &A, &C, peer2)
+
 	// manually check how do the amounts look like
-	log.Println("end")
 	PrintStatus("Pres enter to end the execution.")
 	_, _ = stdinReader.ReadString('\n')
 }
 
+//
 func main() {
 	StartTest()
 }
