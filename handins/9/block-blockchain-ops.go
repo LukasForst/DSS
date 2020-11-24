e0007809d6fecb31e8632c0947ad2ad711860117
Hannah Schaible <h.schaible@campus.lmu.de>
2020-11-24 17:38:58 +0100

    adjusted AppendBlock

--- handins/9/block-blockchain-ops.go
+++ handins/9/block-blockchain-ops.go
@@ -10,8 +10,10 @@ func (bc *BlockChain) AppendBlock(block *Block) {
 
 	bc.Blocks[block.Hash] = block
 	// todo what if that one does not exist?
+	previousBlock := bc.Blocks[block.PreviousBlockHash]
+
 	if block.PreviousBlockHash != "" {
-		previousBlock := bc.Blocks[block.PreviousBlockHash]
+		previousBlock.NextBlocksHashes = append(previousBlock.NextBlocksHashes, previousBlock.Hash)
 	} else {
 		PrintStatus("The previous block hash is empty!")
 	}
